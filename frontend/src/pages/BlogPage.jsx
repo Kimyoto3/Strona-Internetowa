e3397eecda039122261581f60fe1c342264d1260
Kimyoto3 <jandomanski32@gmail.com>
2026-02-08 12:17:37 +0100

    Update BlogPage.jsx

--- frontend/src/pages/BlogPage.jsx
+++ frontend/src/pages/BlogPage.jsx
@@ -1,32 +1,5 @@
-import { useParams } from "react-router-dom";
-import { blogs } from "../data/blogs";
+import BlogList from "./BlogList";
 
 export default function BlogPage() {
-  const { slug } = useParams();
-
-  const blog = blogs.find(b => b.slug === slug);
-
-  if (!blog) {
-    return <h1>Nie znaleziono artykułu</h1>;
-  }
-
-  return (
-    <div style={{ maxWidth: "800px", margin: "0 auto", paddingTop: "100px" }}>
-      <img
-        src={blog.image_url}
-        alt={blog.title}
-        style={{ width: "100%", marginBottom: "24px" }}
-      />
-
-      <p>
-        {blog.category} • {blog.read_time} min czytania
-      </p>
-
-      <h1>{blog.title}</h1>
-
-      <div style={{ whiteSpace: "pre-line" }}>
-        {blog.content}
-      </div>
-    </div>
-  );
+  return <BlogList />;
 }
