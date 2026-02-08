36cc71edbc55181ac9a081fc8d830457912fd8ce
Kimyoto3 <jandomanski32@gmail.com>
2026-02-08 12:28:43 +0100

    Update BlogPage.jsx

--- frontend/src/pages/BlogPage.jsx
+++ frontend/src/pages/BlogPage.jsx
@@ -1,5 +1,10 @@
-import BlogList from "./BlogList";
+import { blogs } from "../data/blogs";
 
 export default function BlogPage() {
-  return <BlogList />;
+  return (
+    <div style={{ paddingTop: 100 }}>
+      <h1>BLOG TEST</h1>
+      <pre>{JSON.stringify(blogs, null, 2)}</pre>
+    </div>
+  );
 }
